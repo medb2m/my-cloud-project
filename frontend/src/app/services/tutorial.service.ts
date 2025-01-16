130095a041fd86d070edff9ec58ab2fe727d8a6b
Mohamed <79202818+medb2m@users.noreply.github.com>
2025-01-16 19:01:12 +0100

    Update tutorial.service.ts

--- frontend/src/app/services/tutorial.service.ts
+++ frontend/src/app/services/tutorial.service.ts
@@ -3,7 +3,7 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Tutorial } from '../models/tutorial.model';
 
-const baseUrl = 'http://backend:8087/api/tutorials';
+const baseUrl = 'http://backend:8089/api/tutorials';
 
 @Injectable({
   providedIn: 'root',
